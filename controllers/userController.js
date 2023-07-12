25d02ea1012967202b3f419bbc0a9d69be1fc894
StephenCodesStuff <122505441+StephenCodesStuff@users.noreply.github.com>
2023-07-12 17:48:32 -0500

    working on routes

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const User = require('../models/user');
+
+module.exports = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async deleteSingleUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: 'No user found with this id' });
+      }
+  
+      console.log(user);
+      res.json({ message: 'User successfully deleted' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,65 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
+
+// * `username`
+//   * String
+//   * Unique
+//   * Required
+//   * Trimmed
+
+// * `email`
+//   * String
+//   * Required
+//   * Unique
+//   * Must match a valid email address (look into Mongoose's matching validation)
+
+// * `thoughts`
+//   * Array of `_id` values referencing the `Thought` model
+
+// * `friends`
+//   * Array of `_id` values referencing the `User` model (self-reference)
+
+// **Schema Settings**:
+
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
